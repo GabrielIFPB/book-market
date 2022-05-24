22abc87e71ff33568e97188f9841846734901fc3
gabriel <jeronimogbariel@gmail.com>
2022-05-23 23:23:37 -0300

    CustomerController improvements to get closer to how it actually works

--- src/main/kotlin/com/inteligenciadigital/bookmarket/controllers/CustomerController.kt
+++ src/main/kotlin/com/inteligenciadigital/bookmarket/controllers/CustomerController.kt
@@ -3,16 +3,20 @@ package com.inteligenciadigital.bookmarket.controllers
 import com.inteligenciadigital.bookmarket.models.Customer
 import org.springframework.web.bind.annotation.*
 import java.time.LocalDate
+import java.time.Month
 
 @RestController
 @RequestMapping("api/customers")
 class CustomerController {
 
 	@GetMapping("")
-	fun findAll(): Customer =
-		Customer("naruto", LocalDate.of(1989, 4, 25))
+	fun findAll(): List<Customer> =
+		arrayListOf(
+			Customer("naruto", LocalDate.of(1989, Month.APRIL, 25)),
+			Customer("Gohan", LocalDate.of(1989, Month.NOVEMBER, 25)),
+		)
 
 	@PostMapping("")
 	fun save(@RequestBody data: Customer): Customer =
-		Customer("Goku", LocalDate.of(1992, 1, 26))
+		Customer("Goku", LocalDate.of(1992, Month.JANUARY, 26))
 }
