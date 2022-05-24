ab193da923a83ee4eaf90262eb9bfc6bee4033b8
gabriel <jeronimogbariel@gmail.com>
2022-05-23 22:02:11 -0300

    user model improvement

--- src/main/kotlin/com/inteligenciadigital/bookmarket/models/User.kt
+++ src/main/kotlin/com/inteligenciadigital/bookmarket/models/User.kt
@@ -1,19 +1,28 @@
 package com.inteligenciadigital.bookmarket.models
 
+import java.util.*
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
-@Entity(name = "user")
+//@Entity(name = "user")
 class User(
-	@Column(name = "name")
+//	@Column(name = "name")
 	var username: String,
-	@Column(name = "email", unique = true)
+//	@Column(name = "email", unique = true)
 	var email: String
 ) {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
 	var id: Long? = null
+
+	@Column(name = "uid", unique = true)
+	var uid: String = ""
+
+	init {
+		val uid: UUID = UUID.randomUUID()
+		this.uid = this.uid.ifEmpty { uid.toString() }
+	}
 }
