2b18c2eb386398a66fcc2a265660a311cc75a7de
gabriel <jeronimogbariel@gmail.com>
2022-05-30 00:16:09 -0300

    fixing the delete customer method in the service module

--- src/main/kotlin/com/inteligenciadigital/bookmarket/service/CustomerServiceImplements.kt
+++ src/main/kotlin/com/inteligenciadigital/bookmarket/service/CustomerServiceImplements.kt
@@ -1,8 +1,10 @@
 package com.inteligenciadigital.bookmarket.service
 
+import com.inteligenciadigital.bookmarket.exception.CustomerNotFoundException
 import com.inteligenciadigital.bookmarket.models.Customer
 import com.inteligenciadigital.bookmarket.repository.CustomerRepository
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 
 @Service
 class CustomerServiceImplements(var repository: CustomerRepository): CustomerService {
@@ -17,11 +19,11 @@ class CustomerServiceImplements(var repository: CustomerRepository): CustomerSer
 		TODO("Not yet implemented")
 	}
 
-	override fun delete(customer: Customer) {
-		TODO("Not yet implemented")
+	override fun delete(id: Long)  {
+		throw CustomerNotFoundException("not found!")
 	}
 
 	override fun findById(id: Long): Customer {
-		TODO("Not yet implemented")
+		return Customer("", LocalDate.now())
 	}
 }
