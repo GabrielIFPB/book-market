e5eabe2f7d3191d93c978feb26f64fa8b98babbf
gabriel <jeronimogbariel@gmail.com>
2022-05-29 21:41:14 -0300

    created model to represent the errors/exceptions

---
+++ src/main/kotlin/com/inteligenciadigital/bookmarket/models/ModelException.kt
@@ -0,0 +1,11 @@
+package com.inteligenciadigital.bookmarket.models
+
+import org.springframework.http.HttpStatus
+import java.sql.Timestamp
+
+class ModelException {
+	private val message: String? = null
+	private val httpStatus: HttpStatus? = null
+	private val status = 0
+	private val timestamp: Timestamp? = null
+}
