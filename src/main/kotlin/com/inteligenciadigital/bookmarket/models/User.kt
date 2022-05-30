448523fa2a4e3b0de5f9732d3efe8f36ca61329c
gabriel <jeronimogbariel@gmail.com>
2022-05-30 00:14:32 -0300

    added equals, hashCode and toString methods in Customer and User models

--- src/main/kotlin/com/inteligenciadigital/bookmarket/models/User.kt
+++ src/main/kotlin/com/inteligenciadigital/bookmarket/models/User.kt
@@ -1,6 +1,7 @@
 package com.inteligenciadigital.bookmarket.models
 
 
+import org.hibernate.Hibernate
 import java.util.UUID
 import javax.persistence.Column
 import javax.persistence.Entity
@@ -9,7 +10,7 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity(name = "user")
-class User(
+data class User(
 	@Column(name = "name")
 	var username: String,
 	@Column(name = "email", unique = true)
@@ -24,5 +25,20 @@ class User(
 	init {
 		val uid: UUID = UUID.randomUUID()
 		this.uid = this.uid.ifEmpty { uid.toString() }
+	}
+
+	override fun equals(other: Any?): Boolean {
+		if (this === other) return true
+		if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+		other as User
+
+		return id != null && id == other.id
+	}
+
+	override fun hashCode(): Int = javaClass.hashCode()
+
+	@Override
+	override fun toString(): String {
+		return this::class.simpleName + "(id = $id , username = $username , email = $email , uid = $uid )"
 	}
 }
