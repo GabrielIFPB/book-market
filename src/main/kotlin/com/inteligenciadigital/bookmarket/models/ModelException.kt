e5121d91165d1730176371d9a0e6aa37d0aebd04
gabriel <jeronimogbariel@gmail.com>
2022-05-29 22:42:43 -0300

    using the builder pattern in the errors/exceptions model

--- src/main/kotlin/com/inteligenciadigital/bookmarket/models/ModelException.kt
+++ src/main/kotlin/com/inteligenciadigital/bookmarket/models/ModelException.kt
@@ -3,9 +3,22 @@ package com.inteligenciadigital.bookmarket.models
 import org.springframework.http.HttpStatus
 import java.sql.Timestamp
 
-class ModelException {
-	private val message: String? = null
-	private val httpStatus: HttpStatus? = null
-	private val status = 0
-	private val timestamp: Timestamp? = null
+class ModelException private constructor(
+	var message: String? = null,
+	var httpStatus: HttpStatus? = null,
+	var status: Int = 0,
+	var timestamp: Timestamp? = null
+) {
+	data class Builder(
+		var message: String? = null,
+		var httpStatus: HttpStatus? = null,
+		var status: Int = 0,
+		var timestamp: Timestamp? = null) {
+
+		fun message(message: String) = apply { this.message = message }
+		fun httpStatus(httpStatus: HttpStatus) = apply { this.httpStatus = httpStatus }
+		fun status(status: Int) = apply { this.status = status }
+		fun timestamp(timestamp: Timestamp) = apply { this.timestamp = timestamp }
+		fun build() = ModelException(message, httpStatus, status, timestamp)
+	}
 }
