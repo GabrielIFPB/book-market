3c8794696be6fea9a4bfd8e9447246190fa5eeee
gabriel <jeronimogbariel@gmail.com>
2022-06-05 11:19:19 -0300

    implementing method to update the date of birth of the customer in the service

--- src/main/kotlin/com/inteligenciadigital/bookmarket/service/CustomerServiceImplements.kt
+++ src/main/kotlin/com/inteligenciadigital/bookmarket/service/CustomerServiceImplements.kt
@@ -5,6 +5,8 @@ import com.inteligenciadigital.bookmarket.exception.CustomerNotFoundException
 import com.inteligenciadigital.bookmarket.models.Customer
 import com.inteligenciadigital.bookmarket.repository.CustomerRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
 
 
 @Service
@@ -12,18 +14,22 @@ class CustomerServiceImplements(var repository: CustomerRepository): CustomerSer
 
 	override fun all(): List<Customer> =
 		this.repository.findAll()
-
     
 	override fun save(customer: Customer): Customer =
 		this.repository.save(customer)
-
     
 	override fun update(customer: Customer): Customer =
 		this.findById(customer.id).let {
-			if (customer.uid != it.uid) throw CustomerBadRequest("not found uid")
+			if (customer.uid != it.uid)
+				throw CustomerBadRequest("not found uid: ${customer.uid}")
 			return this.repository.save(customer)
 		}
 
+	@Transactional
+	override fun updateBirthdate(id: Long, birthdate: LocalDate): Int =
+		this.findById(id).let {
+			this.repository.updateBirthdate(id, birthdate)
+		}
   
 	override fun delete(id: Long): Unit =
 		this.repository.delete(this.findById(id))
